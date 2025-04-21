9d182cdfa7d519be3949827d17222aea11df05cf
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2025-04-21 15:53:56 -0300

    adiciona código para encontrar o livro mais barato e lista de preços

---
+++ listaLivros.js
@@ -0,0 +1,12 @@
+// Exemplo de código para encontrar o livro mais barato
+
+const precosLivros = [
+    {titulo: "Java Script", preco: 25},
+    {titulo: "PHP", preco: 15 },
+    {titulo: "Java", preco:30 },
+    {titulo: "Elixir", preco:50 },
+    {titulo: "Go", preco: 45 },
+    {titulo: "Python", preco:20 }];
+
+ module.exports = precosLivros;   
+
